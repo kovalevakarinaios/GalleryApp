e0c2b967ecaddb3777b088f0da54ef71635bae72
Karina Kovaleva <karina_kovaleva_ios@icloud.com>
2024-10-26 17:42:35 +0200

    CREATE initial setup

---
+++ GalleryApp/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  GalleryApp
+//
+//  Created by Karina Kovaleva on 26.10.24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+
---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Karina Kovaleva on 26.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+    }
+}
