d06b495afe0f78683c1e506b1e8dfd8f949bb468
Karina Kovaleva <karina_kovaleva_ios@icloud.com>
2024-10-27 18:43:46 +0100

    ADDED basic UICollectionView and PhotoCell setup

---
+++ GalleryApp/PhotoCell.swift
@@ -0,0 +1,40 @@
+//
+//  PhotoCell.swift
+//  GalleryApp
+//
+//  Created by Karina Kovaleva on 27.10.24.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    static let identifier = "PhotoCell"
+    
+    private lazy var imageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .cyan
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupImageView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupImageView() {
+        self.contentView.addSubview(self.imageView)
+        
+        NSLayoutConstraint.activate([
+            self.imageView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.imageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            self.imageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 5),
+            self.imageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -5)
+        ])
+    }
+}
--- GalleryApp/ViewController.swift
+++ GalleryApp/ViewController.swift
@@ -9,8 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private lazy var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width: 100, height: 100)
+        
+        let collectionView = UICollectionView(frame: .infinite, collectionViewLayout: flowLayout)
+        collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.identifier)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
+        self.setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        self.view.addSubview(self.collectionView)
+        
+        NSLayoutConstraint.activate([
+            self.collectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.collectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            self.collectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.collectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, 
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.identifier,
+                                                            for: indexPath) as? PhotoCell else {
+            return UICollectionViewCell()
+        }
+        return cell
     }
 }
